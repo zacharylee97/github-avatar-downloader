10befe89f2cf969e691317341cdf46ce46448c07
Zachary Lee <zacharylhlee@gmail.com>
2018-10-01 17:24:17 +0000

    Adds authorization

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,20 @@
 var request = require('request');
+var secrets = require('./secrets');
 
 console.log('Welcome to Github Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+      'Authorization': secrets.GITHUB_TOKEN;
+    }
+  };
+
+  request(url, function(err, res, body) {
+    cb(err, body);
+  });
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
