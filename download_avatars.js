73cb0b6828fdc9b0e2c77b8e5c9b952b12092c58
Zachary Lee <zacharylhlee@gmail.com>
2018-10-01 17:45:50 +0000

    Parse JSON string into an object for callback function to console.log value of each avatar_url

--- download_avatars.js
+++ download_avatars.js
@@ -7,17 +7,24 @@ function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'User-Agent': 'request'
-      'Authorization': secrets.GITHUB_TOKEN;
+      'User-Agent': 'request',
+      'Authorization': secrets.GITHUB_TOKEN
     }
   };
-
-  request(url, function(err, res, body) {
-    cb(err, body);
+  request(options, function(err, res, body) {
+    var result = JSON.parse(body);
+    cb(err, result);
   });
 }
 
 getRepoContributors('jquery', 'jquery', function(err, result) {
+  var output = "";
+  result.forEach(function(element, index) {
+    output += element['avatar_url'];
+    if (index !== result.length - 1) {
+      output += "\n";
+    }
+  })
   console.log("Errors:", err);
-  console.log("Result:", result);
+  console.log("Result:", output);
 });
